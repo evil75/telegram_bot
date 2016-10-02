a99fbf642b5f044a4f9e3b624ccb16a3bc3b312a
evil75 <evil75213@gmail.com>
2016-10-02 15:13:51 +0300

    prepaire for docker

--- creditails_example.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-token='Your telegram bot token'
-dbHost='database hosr (default 127.0.0.1)'
-dbUser='database user'
-dbPass='database password'
-dbTable='table name'
-dbPort='database port (default 5432)'
-dbName='database name'
