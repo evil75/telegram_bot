22d3d7a77c29af3968b1e2520b60f0bfc33e182c
evil75 <evil75213@gmail.com>
2016-10-02 21:16:51 +0300

    add creditails

---
+++ creditails.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+token='Your telegram bot token'
+dbHost='database hosr (default 127.0.0.1)'
+dbUser='postgres'
+dbPass='dbPasswd'
+dbTable="events"
+dbPort='5432'
+dbName='postgres'
